c3712cfc9b5408699aace31bec1d5781d39cf5f5
Karl Smith <98028208+Ksmithprofessional@users.noreply.github.com>
2022-09-30 22:09:11 +0100

    Added woocommerce/jetpack

--- wp-content/plugins/contact-form/js/main.js
+++ wp-content/plugins/contact-form/js/main.js
@@ -7,21 +7,33 @@ const form = document.querySelector('form');
 formSubmit.addEventListener('click', (e) => {
 
     let email = document.querySelector('.email input').value;
-    let emailStar = document.querySelector('.e-star')
+    let emailStar = document.querySelector('.e-star');
+    let emailDiv = document.querySelector('#email');
     const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
     //email regex checks
     if(!email.match(regex)) {
 
         e.preventDefault();
-        document.querySelector('.error').innerHTML = `<i class="far fa-times-circle"></i> Invalid email address`;
+        document.querySelector('.error').innerHTML = `Invalid email address`;
         emailStar.style.color = 'red';
+        emailDiv.style.border = '2px solid red';
         // console.log(email);
         // test to see whether the email address actually shows up on failure
 
+    } else if(email === '') {
+
+        // empty email field checks
+        e.preventDefault();
+        document.querySelector('.error').innerHTML = `Please fill in required fields`;
+        Star.style.color = 'red';
+        emailDiv.style.border = '2px solid red';
+
     } else {
 
+        //returns styling back to default
         emailStar.style.color = 'black';
+        emailDiv.style.border = '1px solid black';
     }
 });
 
@@ -30,16 +42,20 @@ formSubmit.addEventListener('click', (e) => {
     //first name field checks
     let name = document.querySelector('.fname input').value;
     let Star = document.querySelector('.name-star');
+    let nameDiv = document.querySelector('#fname');
 
     if(name === '') {
 
         e.preventDefault();
-        document.querySelector('.error').innerHTML = `<i class="far fa-times-circle"></i> Please fill in required fields`;
+        document.querySelector('.error').innerHTML = `Please fill in required fields`;
         Star.style.color = 'red';
+        nameDiv.style.border = '2px solid red';
 
     } else {
 
+        //returns styling back to default
         Star.style.color = 'black';
+        nameDiv.style.border = '1px solid black';
     }
 });
 
@@ -48,30 +64,19 @@ formSubmit.addEventListener('click', (e) => {
     //message area checks
     let message = document.querySelector('.message textarea').value;
     let Star = document.querySelector('.mess-star');
+    let messageDiv = document.querySelector('#message');
 
     if(message === '') {
 
         e.preventDefault();
-        document.querySelector('.error').innerHTML = `<i class="far fa-times-circle"></i> Please fill in required fields`;
+        document.querySelector('.error').innerHTML = `Please fill in required fields`;
         Star.style.color = 'red';
+        messageDiv.style.border = '2px solid red';
 
     } else {
 
+        //returns styling back to default
         Star.style.color = 'black';
-    }
-});
-
-formSubmit.addEventListener('click', (e) => {
-
-    //email empty checks
-    let email = document.querySelector('.email input').value;
-    let Star = document.querySelector('.e-star');
-
-    if(email === '') {
-
-        e.preventDefault();
-        document.querySelector('.error').innerHTML = `<i class="far fa-times-circle"></i> Please fill in required fields`;
-        Star.style.color = 'red';
-
+        messageDiv.style.border = '1px solid black';
     }
 });
